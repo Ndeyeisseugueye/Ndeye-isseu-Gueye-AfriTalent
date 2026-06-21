6a5c49752fc83d2890d4b109d8289940898ef51f
Ndeyeisseu <gueyendeyeisseu5@gmail.com>
2026-06-21 12:42:22 +0000

    JS : dark mode avec localStorage, navbar dynamique au scroll

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// darkmode
+
+const themeToggle = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    themeToggle.textContent = "☀️";
+}
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        themeToggle.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeToggle.textContent = "🌙";
+    }
+});
+
+
+// bouton retour en haut
+
+const backToTopBtn = document.getElementById('backToTop');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 300) {
+        backToTopBtn.style.display = 'flex';
+    } else {
+        backToTopBtn.style.display = 'none';
+    }
+});
+
+if (backToTopBtn) {
+    backToTopBtn.addEventListener('click', function () {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
